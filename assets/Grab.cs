1a8422e5c0f5603d30442a80adb24970e4bb866e
Pietro <pietroiaia98@gmail.com>
2020-02-01 04:29:41 -0400

    Ahora el Jugador puede agarrar objetos y cambia de direccion

---
+++ assets/Grab.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grab : MonoBehaviour
+{
+    // Variable que checkea si el objeto esta al alcance de ser agarrado
+    private bool reach = false;
+    // Variable que checkea si el objeto fue agarrado
+    public bool grabbed = false;
+    // GameObject del objeto agarrado
+    private GameObject object_grabbed;
+    // velocidad del Jugador
+    [SerializeField]
+    private float speed = 5f;
+    // Direccion de movimiento
+    private float moveHorizontal;
+
+    void Update()
+    {
+        // Si el jugador presiona 'E' y esta al alcance, agarra un objeto
+        if(Input.GetButton("Grab") && reach)
+        {
+            // El objeto se mueve con el Jugador
+            moveHorizontal = Input.GetAxisRaw("Horizontal");
+            grabbed = true;
+            // Actualizamos la posicion
+            object_grabbed.transform.position += new Vector3(speed * Time.deltaTime * moveHorizontal, 0f, 0f);
+        }
+        // Si se suelta la 'E', el Jugador suelta el objeto
+        if(Input.GetButtonUp("Grab"))
+        {
+            grabbed = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        // Si collisiona con el Objeto, toma su GameObject y esta al alcance de ser agarrado
+        if(col.tag == "Obstacle")
+        {
+            object_grabbed = col.gameObject;
+            reach = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        // Si se aleja del trigger, el objeto ya no esta al alcance de ser agarrado
+        // O se suelta
+        if(col.tag == "Obstacle")
+        {
+            grabbed = false;
+            reach = false;
+        }
+    }
+}
--- assets/movement.cs
+++ assets/movement.cs
@@ -9,6 +9,15 @@ public class movement : MonoBehaviour
     private float speed = 5f;
     // Direccion de movimiento
     private float moveHorizontal;
+    public GameObject hand;
+    private Grab grab;
+    private bool grabbed;
+
+    void Start()
+    {
+        // Obtenemos el componente 'Grab' de el GameObject hand
+        grab = hand.GetComponent<Grab>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -16,5 +25,15 @@ public class movement : MonoBehaviour
         moveHorizontal = Input.GetAxisRaw("Horizontal");
         // Actualizamos la posicion
         transform.position += new Vector3(speed * Time.deltaTime * moveHorizontal, 0f, 0f);
+        grabbed = grab.grabbed;
+        // Si se mueve en direccion contraria y no tiene nada agarrado, rota 180 grados
+        if((moveHorizontal < 0) && !grabbed)
+        {
+            transform.rotation = Quaternion.Euler(Vector3.up * 180);
+        }
+        if((moveHorizontal > 0) && !grabbed)
+        {
+            transform.rotation = Quaternion.Euler(Vector3.up * 0);
+        }
     }
 }
