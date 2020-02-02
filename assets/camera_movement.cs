23eff1dddfc9d509e9a3f5afa2ecec951ff106b4
Pietro <pietroiaia98@gmail.com>
2020-02-02 08:35:17 -0400

    Camara sigue al jugador bien

---
+++ assets/camera_movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera_movement : MonoBehaviour
+{
+    public GameObject player;
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x <= 0)
+        {
+            transform.position = new Vector3(0, -0.8699996f, -10);
+        }
+        if(transform.position.x <= player.transform.position.x){
+            transform.position = (Vector3.right * (player.transform.position.x)) + (Vector3.up * (-0.8699996f)) + new Vector3(0, 0, -10);
+        }
+        else if(transform.position.x >= player.transform.position.x && transform.position.x > 0)
+        {
+            transform.position = (Vector3.right * (player.transform.position.x)) + (Vector3.up * (-0.8699996f)) + new Vector3(0, 0, -10);
+        }
+    }
+}
