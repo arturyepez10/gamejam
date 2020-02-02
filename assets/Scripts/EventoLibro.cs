256b175f964182d8391d283a056c7e422afaf790
Arturo <arturyepez10@gmail.com>
2020-02-02 12:21:22 -0400

    mucho

---
+++ assets/Scripts/EventoLibro.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EventoLibro : MonoBehaviour
+{
+
+    private Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.name == "Floor")
+        {
+            rb2d.constraints = RigidbodyConstraints2D.FreezeAll;
+        }
+    }
+}
